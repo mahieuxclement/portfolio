d2c50538be40b0bdc3be0993d35439a2c8e0b7ef
clement_mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2024-01-26 16:46:51 +0100

    test correction export8

--- app/Competances/page.tsx
+++ app/Competances/page.tsx
@@ -3,7 +3,7 @@ import Footer from "../Footer";
 import {CompetanceComponent} from "./CompetanceBox";
 import { CompetancesData } from "../data";
 
-export const competances = () => {return (
+function competances() {return (
     <main className='min-h-screen flex-col bg-neutral-900'>
         <Navbar/>
         <div className='grid grid-flow-row-dense grid-cols-2 mt-24 space-x-3 mx-auto px-12 py-4 max-[900px]:grid-cols-1 min-[1400px]:grid-cols-3'>
