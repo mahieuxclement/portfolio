8a459a2c18298cecba0277b75c7c2102dc9f29a1
clement_mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2024-01-26 15:24:31 +0100

    correction experience composant

--- app/Experiences/page.tsx
+++ app/Experiences/page.tsx
@@ -1,13 +1,13 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import ExperienceComponent from "../experiencesComponent/page";
-import {ExperienceContent} from "../data"
+import {ExperienceData} from "../data"
 
 export const Experience = () => {return (
     <main className='min-h-screen flex-col bg-neutral-900'>
         <Navbar/>
         <div id="contentBody" className='items-center grid grid-flow-row-dense grid-cols-2 mt-24 mx-auto px-12 py-4 max-[900px]:grid-cols-1'>
-            {ExperienceContent.map((data) => (
+            {ExperienceData.map((data) => (
                 <ExperienceComponent title={data.title} description={data.content}/>
             ))}
         </div>
