1d3a737715747ce80cf22c07dd20e14bee591187
clement_mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2024-01-26 15:34:12 +0100

    ajout des key pour le deploiment

--- app/Projets/page.tsx
+++ app/Projets/page.tsx
@@ -7,8 +7,10 @@ export const projets = () => {return (
     <main className='min-h-screen flex-col bg-neutral-900'>
         <Navbar/>
         <div id="contentBody" className='items-center grid grid-flow-row-dense grid-cols-2 mt-24 mx-auto px-12 py-4 max-[900px]:grid-cols-1'>
-        {ProjetData.map((data) => (
+        {ProjetData.map((data,index) => (
+            <li key={index} className="list-none">
                 <ProjetComponent title={data.title} techno={data.techno} description={data.content}/>
+                </li>
             ))}
         </div>
         <Footer/>
